255b98e47b2eb82985d0a7d90c8303f0738fc67e
Leay15 <ivan_yael_gp@outlook.com>
2017-11-22 21:30:36 -0600

    Correccion del Repositorio

---
+++ app/src/main/java/com/combitracker/user/Objetos/Combi.java
@@ -0,0 +1,60 @@
+package com.combitracker.user.Objetos;
+
+/**
+ * Created by juamp on 18/11/2017.
+ */
+
+public class Combi {
+private String Usuarui,Lat,Lon,Contraseña,RutaAsignada,Numero;
+
+    public Combi() {
+    }
+
+    public String getUsuarui() {
+        return Usuarui;
+    }
+
+    public void setUsuarui(String usuarui) {
+        Usuarui = usuarui;
+    }
+
+    public String getLat() {
+        return Lat;
+    }
+
+    public void setLat(String lat) {
+        Lat = lat;
+    }
+
+    public String getLon() {
+        return Lon;
+    }
+
+    public void setLon(String lon) {
+        Lon = lon;
+    }
+
+    public String getContraseña() {
+        return Contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        Contraseña = contraseña;
+    }
+
+    public String getRutaAsignada() {
+        return RutaAsignada;
+    }
+
+    public void setRutaAsignada(String rutaAsignada) {
+        RutaAsignada = rutaAsignada;
+    }
+
+    public String getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(String numero) {
+        Numero = numero;
+    }
+}
---
+++ app/src/main/java/com/combitracker/user/Objetos/Subrutas.java
@@ -0,0 +1,28 @@
+package com.combitracker.user.Objetos;
+
+/**
+ * Created by juamp on 19/11/2017.
+ */
+
+public class Subrutas {
+    private String camino,ruta;
+
+    public String getCamino() {
+        return camino;
+    }
+
+    public void setCamino(String camino) {
+        this.camino = camino;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public Subrutas() {
+    }
+}
